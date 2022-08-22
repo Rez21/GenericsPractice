ee779452c5209f61ce2bf2227a7b807d59384b9d
Prasad <banprasad21@gmail.com>
2022-08-23 01:53:23 +0530

    [Raghav] Add: generic concept practice

---
+++ GenericConcept/GenericMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericConcept
+{
+    internal class GenericMethod
+    {
+        public static void toPrint<I>(I[] inputArray)
+        {
+            foreach(var element in inputArray)
+            {
+               Console.WriteLine(element);
+            }
+            Console.WriteLine("------------------------------------------------------------");
+        }
+    }
+}
---
+++ GenericConcept/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4 };
+            double[] doubleArray = { 1.2, 1.3, 1.4 };
+            char[] charArray = { 'A', 'B', 'C' };
+
+
+            Generic.toPrint(intArray);
+            Generic.toPrint(doubleArray);
+            Generic.toPrint(charArray);
+
+            // Using Generic Method concept
+            GenericMethod.toPrint<int>(intArray);
+            GenericMethod.toPrint<double>(doubleArray);
+            GenericMethod.toPrint<char>(charArray);
+
+            // Using Generic Class concept
+
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
+        }
+    }
+}
